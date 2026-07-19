ff9d5e886ec43267be47cd05de7c0c6ad2d9fc16
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-19 21:18:45 +0000

    Create variables

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,3 +1,11 @@
 public class CurrencyConverter {
-    
+    int rupee = 63;
+    int dirham = 3; 
+    int real = 3;       
+    int chilean_peso = 595;
+    int mexican_peso = 18;       
+    int _yen = 107;
+    int $australian = 2;  
+    int dollar; 
+    int Rupee = 63;
 }
