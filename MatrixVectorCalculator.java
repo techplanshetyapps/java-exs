8ac3ec9fd053376d5885a1752bda9235832b599b
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-20 05:12:31 +0000

    Create methods

--- MatrixVectorCalculator.java
+++ MatrixVectorCalculator.java
@@ -1,3 +1,26 @@
 public class MatrixVectorCalculator {
+
+    public static int dotProduct(int[] a, int[] b) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+        }
+        return sum;
+    }
+
     
+    public static int[] multiplyMatrixVector(int[][] matrix, int[] vector) {
+        int[] result = new int[matrix.length];
+        
+    
+        for (int i = 0; i < matrix.length; i++) {
+            int sum = 0;
+            
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j] * vector[j];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
 }
