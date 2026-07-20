49bd03c50ca99055b9f0b83f102c9e1ec131a62c
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-20 05:02:21 +0000

    Create an execute method

--- CurrencyService.java
+++ CurrencyService.java
@@ -10,4 +10,14 @@ public class CurrencyService {
         return transferAmount * 0.02;
     }
 
+    public static void main(String[] args) {
+        MoneyTransferService transferService = new MoneyTransferService();
+        
+        double transferAmount = transferService.computeTransferAmount(0, 1000);
+        double transferFee = transferService.computeTransferFee(0, 1000);
+        
+        System.out.println("Transfer Amount: " + transferAmount);
+        System.out.println("Transfer Fee: " + transferFee);
+    }
+
 }
