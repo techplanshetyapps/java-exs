b6aa7202aa9d2d8429058660de067cec1b28ee3f
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-26 14:59:09 +0000

    Create an method queryFrequency compute

--- IRUtil.java
+++ IRUtil.java
@@ -1,6 +1,20 @@
 public class IRUtil {
     public static int getFrequencyCount(String query, String doc) {
-    }
+   
+            query = query.toLowerCase();
+            doc = doc.toLowerCase();
+            
+            int frequencyCount = 0;
+            int index = doc.indexOf(query);
+
+            while (index >= 0) {
+                frequencyCount++;
+                doc = doc.substring(index + query.length());
+                index = doc.indexOf(query);
+            }
+            
+            return frequencyCount;
+        }
     public static double queryFrequency(String query, String doc) {
     }
 }
